557531cf249751929edeacec60a957b8e3071758
Savera <Level 2>
2015-09-09 18:30:50 -0700

    Found all the Animal Hipsters

--- src/AnimalHipster.java
+++ src/AnimalHipster.java
@@ -6,15 +6,40 @@ public class AnimalHipster {
 	
 
 	public ArrayList<String> findAnimalHipsters(HashMap<String, ArrayList<String>> network, HashMap<String, String> favoriteAnimal){
+	
+		ArrayList<String> animalHipster = new ArrayList<String>();
+	
 		
-		
-	for(String person : favoriteAnimal.values()){
-		
-		System.out.println(person);
-		
-	}
-	return null;
-		 
+		for (String s : favoriteAnimal.keySet()) {
+			
+			boolean match = false;
+			
+			String p = favoriteAnimal.get(s);
+			
+			for(String b : network.get(s)){ 
+				
+				String f = favoriteAnimal.get(b); 
+				
+				if (favoriteAnimal.get(b).equals(p)){
+					
+					match = true; 
+					
+					break; 
+					
+				}
+
+			}
+			
+			if(!match){
+				
+				animalHipster.add(s);
+				
+			}
+			
+			System.out.println(animalHipster);
+		}	
+	
+	return animalHipster; 
 	}
 	
 }
